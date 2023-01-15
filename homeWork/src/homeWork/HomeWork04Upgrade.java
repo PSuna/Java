a333c67680a3853a35f7ef3c2463710b0ae45b32
Suna <xkq3355@naver.com>
2023-01-15 23:45:46 +0900

    2023-1-15 집에서

--- homeWork/src/homeWork/HomeWork04Upgrade.java
+++ homeWork/src/homeWork/HomeWork04Upgrade.java
@@ -5,14 +5,9 @@ import java.util.Scanner;
 public class HomeWork04Upgrade {
 	
 	static Scanner sc = new Scanner(System.in);
-	static Product[] product = null;
-	int count = 0;
-	int max = 0;
-	int maxIndex = 0;
 	
 	// 함수로 바꾸기 
 	public static int menuSelect() {
-		Scanner sc = new Scanner(System.in);
 		System.out.println("=======================================================");
 		System.out.println("1.상품 수 | 2.상품 및 가격입력 | 3.제품별 가격 | 4.분석 | 5.종료");
 		System.out.println("=======================================================");
@@ -22,10 +17,57 @@ public class HomeWork04Upgrade {
 		return menu;
 	}
 	
-//	public static int input() {
-//		System.out.print("상품수를 입력하세요> ");
-		//count = Integer.parseInt(sc.nextLine());
-//	}
+	public static int inputProductNo() {
+		System.out.print("상품수를 입력하세요> ");
+		int count = Integer.parseInt(sc.nextLine());
+		return count;
+	}
+	
+	public static Product[] insertProduct(int count, Product[] product) {
+		product = new Product[count];
+		for(int i =0; i<product.length; i++) {
+			
+			System.out.println("상품명을 입력하세요> ");
+			String proName = sc.nextLine();
+			
+			System.out.println("가격을 입력하세요> ");
+			int price = Integer.parseInt(sc.nextLine());
+
+			product[i] = new Product(proName, price);
+		}
+		return product;
+	}
+	
+	public static void print(Product[] product) {
+		for(int i = 0; i<product.length; i++) {
+			System.out.println(product[i].proName + " : " + product[i].price);
+		}
+	}
+	
+	public static void totalSum(Product[] product) {
+		int sum = 0; // 총합
+		int max = product[0].price; // 초기화
+		int maxIndex = 0;
+		
+		for(int i = 0 ; i < product.length; i++) {
+			if(max < product[i].price) {
+				max = product[i].price;
+				maxIndex = i;
+			}
+		}
+		System.out.println("최고가격을 가지는 제품명: " + product[maxIndex].proName +", 가격: " + max);
+		
+		for(int i = 0; i < product.length; i++) {
+			if(i == maxIndex) { // 최고가격을 가지는 제품의 인덱스라면
+				continue;  // 건너뛰기
+			}
+			sum += product[i].price;
+			
+		}
+		System.out.println("최고 가격을 가지는 제품을 제외한 제품들의 총합 : " + sum);
+		
+	}
+	
 	
 	public static void main(String[] args) {
 		// 문제2) 다음은 키보드로부터 상품 수와 상품 가격을 입력받아서
@@ -40,54 +82,29 @@ public class HomeWork04Upgrade {
 		// 4) 분석기능은 최고 가격을 가지는 제품과 해당 제품을 제외한 제품들의 총합을 구합니다.
 		// 5) 종료 시에는 프로그램을 종료한다고 메세지를 출력하도록 구현하세요.
 		
-		
+		Product[] product = null;
+		int count = 0;
 		
 		while(true) {
 			
 			int menu = menuSelect();
 			
 			if(menu == 1) {
-				// 함수
-				System.out.print("상품수를 입력하세요> ");
-				count = Integer.parseInt(sc.nextLine());
+				
+				count = inputProductNo();
 				
 			}else if(menu == 2) {
-				product = new Product[count];
-				for(int i =0; i<product.length; i++) {
-					
-					System.out.println("상품명을 입력하세요> ");
-					String proName = sc.nextLine();
-					
-					System.out.println("가격을 입력하세요> ");
-					int price = Integer.parseInt(sc.nextLine());
-
-					product[i] = new Product(proName, price);
-				}
+				
+				product = insertProduct(count, product);
+				
 			}else if(menu == 3) {
-				for(int i = 0; i<product.length; i++) {
-					System.out.println(product[i].proName + " : " + product[i].price);
-				}
+				
+				print(product);
+				
 			}else if(menu == 4) {
 				//최고 가격을 가지는 제품과 해당 제품을 제외한 제품들의 총합
 				//분석기능은 최고 가격을 가지는 제품과 해당 제품을 제외한 제품들의 총합을 구합니다.
-				int sum = 0; // 총합
-				max = product[0].price; // 초기화
-				for(int i = 0 ; i < product.length; i++) {
-					if(max < product[i].price) {
-						max = product[i].price;
-						maxIndex = i;
-					}
-				}
-				System.out.println("최고가격을 가지는 제품명: " + product[maxIndex].proName +", 가격: " + max);
-				
-				for(int i = 0; i < product.length; i++) {
-					if(i == maxIndex) { // 최고가격을 가지는 제품의 인덱스라면
-						continue;  // 건너뛰기
-					}
-					sum += product[i].price;
-					
-				}
-				System.out.println("최고 가격을 가지는 제품을 제외한 제품들의 총합 : " + sum);
+				totalSum(product);
 				
 			}else if(menu == 5) {
 				System.out.println("프로그램을 종료합니다");
